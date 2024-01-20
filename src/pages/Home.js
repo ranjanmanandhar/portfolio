08dac66fc3666553d654b536ddf44e6a94fe3207
Ranjan Manandhar <ranjan@ranjan.local>
2024-01-20 20:06:47 +0545

    remove unwanted code

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,40 +6,10 @@ import Profile from "./Profile";
 
 function Home() {
   return (
-    // <div className="home" id="/">
-    //   <div className="about">
-    //     <h2> Hi, my name is Ranjan</h2>
-    //     <div className="promt">
-    //       <p>A Software Engineer with a apassion for learning and creating s</p>
-    //       <Email />
-    //       <LinkedIn />
-    //       <GitHub />
-    //     </div>
-    //   </div>
-    //   <div className="skills">
-    //     <h1>Skills</h1>
-    //     <ol className="list">
-    //       <li className="item">
-    //         <h2>FrontEnd</h2>
-    //         <span>HTML, CSS, JS, REACTJS, VUEJS</span>
-    //       </li>
-    //       <li className="item">
-    //         <h2>Backend</h2>
-    //         <span>
-    //           GO, PHP, NODEJS, GRAPHQL, MONGODB, MYSQL, POSTGRESQL, REDIS
-    //         </span>
-    //       </li>
-    //       <li className="item">
-    //         <h2>Languages</h2>
-    //         <span>ENGILIS, NEPALI, NEWARI</span>
-    //       </li>
-    //     </ol>
-    //   </div>
-    // </div>
-    <>
+    <div className="home" id="/">
       <Profile />
       <Contact />
-    </>
+    </div>
   );
 }
 
