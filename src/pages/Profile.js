de9b6c766409eb6f397051a985508de2cfce1e46
Ranjan Manandhar <ranjan@ranjan.local>
2024-07-23 18:24:53 +1000

    add new CV

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,6 +3,7 @@ import "../styles/Profile.css";
 import myImage from "../assets/photos/profile.jpeg";
 import { Link } from "react-scroll";
 import { ReactSocialMediaIcons } from "react-social-media-icons";
+import CV from "../../src/assets/files/Ranjan.pdf";
 
 function Profile() {
   return (
@@ -19,7 +20,9 @@ function Profile() {
           <p>Full Stack Developer</p>
         </div>
         <div className="btn-contaier">
-          <button className="btn btn-color-2">Download CV</button>
+          <a href={CV} download="Ranjan.pdf" target="_blank">
+            <button className="btn btn-color-2">Download CV</button>
+          </a>
           <button className="btn btn-color-1">
             <Link to="contact" smooth={true} offset={40} duration={500}>
               Contact Info
@@ -30,13 +33,13 @@ function Profile() {
           <ReactSocialMediaIcons
             borderColor="#FFFFFF"
             icon="linkedin"
-            url="https://some-website.com/my-social-media-url"
+            url="https://www.linkedin.com/in/ranjan-manandhar-39554a186/"
             size="30"
           />
           <ReactSocialMediaIcons
             borderColor="#FFFFFF"
             icon="github"
-            url="https://some-website.com/my-social-media-url"
+            url="https://github.com/ranjanmanandhar"
             size="30"
           />
         </div>
